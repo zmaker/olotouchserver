54fdc93bc172cad88e77aa769f55a8357904ac1a
zeppelinmaker <paolo.aliverti@gmail.com>
2015-10-26 16:02:36 +0100

    prima release

---
+++ client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ('192.168.3.1', 5150)
+LOOP = True
+
+try:
+	while LOOP:
+		message = raw_input('msg:')
+		
+		if message == "q":
+			LOOP = False
+			
+		# Send data
+		print >>sys.stderr, 'sending "%s"' % message
+		sent = sock.sendto(message, server_address)
+
+		# Receive response
+		print >>sys.stderr, 'waiting to receive'
+		data, server = sock.recvfrom(4096)
+		print >>sys.stderr, 'received "%s"' % data
+
+finally:
+	print >>sys.stderr, 'closing socket'
+	sock.close()
